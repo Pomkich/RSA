5a88f3002168a679b1ab98a57d09c3a88f217eae
Роман Смирнов <pombl4.01@mail.ru>
2022-09-23 18:59:05 +0300

    added file encryption

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -90,10 +90,11 @@ std::string convert_to_bits(int number, int block_len) {
 	return bits;
 }
 
+
 std::string convert_to_bytes(std::string bits) {
 	std::string res;
 	for (int i = 0; i < bits.size() / 8; i++) {
-		unsigned char byte = NULL;
+		char byte = NULL;
 		for (int j = 0; j < 8; j++) {
 			bool bit = (bits[8 * i + j] == '1' ? true : false);
 			byte |= bit << (8 - (j + 1));	// при первой итерации не будет смещения, поэтому +1
@@ -104,6 +105,7 @@ std::string convert_to_bytes(std::string bits) {
 	return res;
 }
 
+// преобразует поток битов в поток чисел
 std::vector<int> convert_to_nums(std::string bits, int blocks, int block_len) {
 	std::vector<int> res;
 	for (int i = 0; i < blocks; i++) {
@@ -143,14 +145,138 @@ std::string decrypt_block(int encrypted_block, int block_len, int exp, int modul
 	return convert_to_bits(m, block_len);	// и преобразуем его в поток битов
 }
 
+void generate_keys() {
+	int p = 5323;
+	int q = 12343;
 
+	std::cout << "enter two different prime numbers" << std::endl;
+	std::cin >> p >> q;
+
+	p = 5323;
+	q = 12343;
+
+	long long n = p * q;	// модуль
+
+	long long Fn = (p - 1) * (q - 1);
+	int e = 0;
+	int d = 0;
+
+	srand(time(NULL));
+	while (d < 2) {	// d обязательно должно быть положительным?
+		e = rand() % Fn + 1;
+		// подбор взаимно простого числа для F(n)
+		while (true) {
+			e++;
+			if (e >= n) e = rand() % Fn + 1;	// если дошли до верхнего предела, снова выбираем число
+			if (gcd(Fn, e) == 1) break;	// если НОД == 1 -> простое число найдено
+		}
+		d = gcd_ext(Fn, e).second;	// d - коэффициент перед e
+	}
+
+	std::cout << "open key:   (" << e << ", " << n << ")" << std::endl;
+	std::cout << "secret key: (" << d << ", " << n << ")" << std::endl;
+}
+
+void encrypt_file(std::string file_name, int e, int n) {
+	std::ifstream input(file_name, std::ios::binary);
+	std::string file_bytes(std::istreambuf_iterator<char>(input), {});
+	int block_len = std::ceil(std::log(n) / std::log(2));
+
+	std::string bits = convert_to_bits(file_bytes, block_len);
+
+	int blocks = count_blocks(bits, block_len);
+	
+	std::vector<int> encrypted_blocks;	// зашифрованное сообщение представлено набором чисел
+	for (int i = 0; i < blocks; i++) {	// вычисляем для каждого блока соответствующее число
+		int e_block = encrypt_block(bits, block_len, i, e, n);
+		encrypted_blocks.push_back(e_block);
+	}
+
+	std::string encrypted_bits;
+	for (int i = 0; i < blocks; i++) {
+		encrypted_bits += convert_to_bits(encrypted_blocks[i], block_len);
+	}
+
+	std::string encrypted_file_bytes = convert_to_bytes(encrypted_bits);
+
+	std::ofstream output("encrypted_" + file_name, std::ios::binary);
+	output << encrypted_file_bytes;
+
+	input.close();
+	output.close();
+
+	//std::cout << convert_to_bits(file_bytes, block_len) << std::endl;
+}
+
+void decrypt_file(std::string file_name, int d, int n) {
+	std::ifstream input(file_name, std::ios::binary);
+	std::string file_bytes(std::istreambuf_iterator<char>(input), {});
+	int block_len = std::ceil(std::log(n) / std::log(2));
+
+	std::string encrypted_bits = convert_to_bits(file_bytes, block_len);
+	
+	int blocks = count_blocks(encrypted_bits, block_len);
+
+	auto encrypted_blocks = convert_to_nums(encrypted_bits, blocks, block_len);
+
+	std::string decrypted_blocks;
+	for (int i = 0; i < encrypted_blocks.size(); i++) {
+		decrypted_blocks += decrypt_block(encrypted_blocks[i], block_len, d, n);
+	}
+
+	//std::string decrypted_bits = convert_to_bits(decrypted_blocks, block_len);
+	std::string decrypted_file_bytes = convert_to_bytes(decrypted_blocks);
+
+	std::ofstream output("decrypted_" + file_name, std::ios::binary);
+	output << decrypted_file_bytes;
+
+	input.close();
+	output.close();
+}
 
 int main() {
-	int p = 23;	// DEBUG -> числа должны выбираться рандомно
-	int q = 2579;
-	int n = p * q;	// модуль
+	char cmd;
+	while (true) {
+		std::cin >> cmd;
+		std::string file_name;
+		int e(0), d(0), n(0);
+		switch (cmd) {
+		case 'g':
+			generate_keys();
+			break;
 
-	int Fn = (p - 1) * (q - 1);
+		case 'e':
+			std::cout << "which file encrypt?" << std::endl;
+			cin >> file_name;
+			std::cout << "enter public key please" << std::endl;
+			cin >> e >> n;
+			encrypt_file(file_name, e, n);
+			std::cout << "file encrypted" << std::endl;
+			break;
+
+		case 'd':
+			std::cout << "which file decrypt?" << std::endl;
+			cin >> file_name;
+			std::cout << "enter secret key please" << std::endl;
+			cin >> d >> n;
+			decrypt_file(file_name, d, n);
+			std::cout << "file decrypted" << std::endl;
+			break;
+
+		case 'q':
+			exit(0);
+			break;
+		}
+	}
+
+	return 0;
+}
+
+/*int p = 5323;	// DEBUG -> числа должны выбираться рандомно
+	int q = 12343;
+	long long n = p * q;	// модуль
+
+	long long Fn = (p - 1) * (q - 1);
 	int e = 0;
 	int d = 0;
 
@@ -183,8 +309,10 @@ int main() {
 		int e_block = encrypt_block(bits, block_len, i, e, n);
 		encrypted_blocks.push_back(e_block);
 	}
+	// с этого момента сообщение зашифровано, его можно передать
 	std::cout << std::endl;
 
+	// представляем набор чисел как набор битов
 	std::string encrypted_bits;
 	for (auto block : encrypted_blocks) {
 		encrypted_bits += convert_to_bits(block, block_len);
@@ -200,6 +328,7 @@ int main() {
 	std::cout << "crypted:   " << encrypted_bits << std::endl;
 	std::cout << "decrypted: " << decrypted_bits << std::endl;
 
+	// если передали не набор чисел а строку битов, то преобразуем их в поток чисел
 	auto new_blocks = convert_to_nums(encrypted_bits, blocks, block_len);
 	for (auto block : encrypted_blocks) {
 		std::cout << block << " ";
@@ -217,10 +346,4 @@ int main() {
 	}
 	std::cout << std::endl;
 	str = convert_to_bytes(str);
-	std::cout << str << std::endl;
-
-	//std::cout << convert_to_bytes(bits) << std::endl;
-	//std::cout << convert_to_bytes(decrypted_bits) << std::endl;
-
-	return 0;
-}
+	std::cout << str << std::endl;*/
